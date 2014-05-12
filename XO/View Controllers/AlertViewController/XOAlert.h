88fa427de450d6c0d554363009090ce507772232
Misha <misha2g@m2g.local>
2014-05-12 15:29:39 +0300

    Додав алерт

---
+++ XO/View Controllers/AlertViewController/XOAlert.h
@@ -0,0 +1,14 @@
+//
+//  XOAlert.h
+//  XO
+//
+//  Created by Misha on 12.05.14.
+//  Copyright (c) 2014 mobilez365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XOAlert : UIViewController
+- (XOAlert *) initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelTitle otherButtonTitles:(NSString *)oserTitle, ...;
+
+@end
