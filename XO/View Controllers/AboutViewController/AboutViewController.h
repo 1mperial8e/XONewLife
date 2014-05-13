e5ac86f1b10027d202ba17aa739840d725a43604
Stas Volskyi <Admin@Admins-Mac.local>
2014-05-13 11:17:23 +0300

    added about screen

---
+++ XO/View Controllers/AboutViewController/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  XO
+//
+//  Created by Stas Volskyi on 13.05.14.
+//  Copyright (c) 2014 mobilez365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
