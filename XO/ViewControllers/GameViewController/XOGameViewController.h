d526be0935f7a569d7fac9c0fa3247dc098c6dd3
Kirill Gorbushko <kiril.gorbushko@thinkmobiles.com; kirill.ge@gmail.com>
2015-10-26 18:11:00 +0200

    removed some files, updated structure of projects

---
+++ XO/ViewControllers/GameViewController/XOGameViewController.h
@@ -0,0 +1,14 @@
+//
+//  TTGameViewController.h
+//  XO
+//
+//  Created by Stas Volskyi on 30.04.14.
+//  Copyright (c) 2014 - present Thinkmobiles. All rights reserved.
+//
+
+#import "XOGameFieldViewController.h"
+
+@interface XOGameViewController : UIViewController
+
+@property (weak, nonatomic) XOGameFieldViewController *gameFieldViewController;
+@end
