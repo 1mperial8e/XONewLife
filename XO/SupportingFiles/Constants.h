8e05b982b1e59444128c45edb1a239e73875cc98
Stas Volskyi <stas.volskiy@thinkmobiles.com>
2015-10-27 17:11:09 +0200

    constants refactor

--- XO/SupportingFiles/Constants.h
+++ XO/SupportingFiles/Constants.h
@@ -1,55 +1,44 @@
-//
-//  Constants.h
 //  XO
+//  Constants.h
+//  
 //
 //  Created by Stas Volskyi on 04.05.14.
 //  Copyright (c) 2014 - present Thinkmobiles. All rights reserved.
 //
 
-//UserDefaults keys
-#define GAME_MODE_SETTING_KEY @"GameDifficultySetting"
-#define GAME_MUSIC_SETTING_KEY @"MusicSetting"
-#define GAME_SOUND_SETTING_KEY @"MusicSetting"
-
-
-
-#define EASY_VICTORY @"easyVictory"
-#define MEDIUM_VICTORY @"mediumVictory"
-#define HARD_VICTORY @"hardVictory"
-#define EASY_LOOSES @"easyLooses"
-#define MEDIUM_LOOSES @"mediumLooses"
-#define HARD_LOOSES @"hardLooses"
-#define MULTIPLAYER_GAMES @"multiplayerGames"
-
+static NSString *const AIDifficultyKey = @"AIDifficultyKey";
+static NSString *const MusicSettingsKey = @"MusicSettingsKey";
+static NSString *const SoundSettingsKey = @"SoundSettingsKey";
 
 #pragma mark - Enums
-typedef enum{
-    XOGameModeSingle,
-    XOGameModeMultiplayer,
-    XOGameModeOnline
-} XOGameMode;
 
-typedef enum {
-    XOPlayerFirst = 1,
-    XOPlayerSecond = -1,
-    XOPlayerNone = 0
-} XOPlayer;
+typedef NS_ENUM(NSInteger, GameMode){
+    GameModeSingle,
+    GameModeMultiplayer,
+    GameModeOnline
+};
 
-typedef enum {
-    XOVectorTypeHorisontal,
-    XOVectorTypeVertical,
-    XOVectorTypeDiagonalLeft,
-    XOVectorTypeDiagonalRight
-} XOVectorType;
+typedef NS_ENUM(NSInteger, Player){
+    PlayerNone,
+    PlayerFirst,
+    PlayerSecond
+};
 
-typedef enum {
-    XOAIGameModeEasy,
-    XOAIGameModeMedium,
-    XOAIGameModeHard
-}XOAIGameMode;
+typedef NS_ENUM(NSInteger, VectorType){
+    VectorTypeHorisontal,
+    VectorTypeVertical,
+    VectorTypeDiagonalLeft,
+    VectorTypeDiagonalRight
+};
 
-typedef enum{
-    NewGameMessageUnknown,
-    NewGameMessageYes,
-    NewGameMessageNo
-}NewGameMessage;
+typedef NS_ENUM(NSInteger, AILevel){
+    AILevelEasy,
+    AILevelMedium,
+    AILevelHard
+};
+
+typedef NS_ENUM(NSInteger, LobbyMessage){
+    LobbyMessageUnknown,
+    LobbyMessageYes,
+    LobbyMessageNo
+};
